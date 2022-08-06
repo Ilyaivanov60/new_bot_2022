2304f00ea6f8f7db23fe2d28ef1df45bcced58f6
ILya Ivanov <ivanovrocket@gmail.com>
2022-08-06 14:54:58 -0400

    new shiny bot

--- mybot.py
+++ mybot.py
@@ -1,20 +1,33 @@
-from telegram.ext import Updater, CommandHandler
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-PROXY = {'proxy_url': 'socks5://t2.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
 def greet_user(update, context):
     print('Вызван /start')
     print(update)
     update.message.reply_text('Здравствую, пользователь!')
 
+def tolk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
 def main():
-    mybot = Updater('5369400699:AAHiX9bTulpUaAc8PHB0CAqr0In5ZxP6jP8', use_context=True) #request_kwargs=PROXY
+    mybot = Updater(settings.API_KEY, use_context=True) #request_kwargs=PROXY
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, tolk_to_me))
 
+    logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
 
-main()
+if  __name__ == '__main__':
+    main()
